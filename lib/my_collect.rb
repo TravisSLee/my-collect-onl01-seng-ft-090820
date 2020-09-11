ce678dbe2e955b644f8463452f5edb0f01c6bfcb
Travis Lee <travisslee223@gmail.com>
2020-09-11 20:14:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,8 @@ def my_collect(array)
   i = 0 
   new_array = []
   while i < array.length 
-    yield array[i]
+    new_array.push(yield array[i])
     i = i + 1
-    new_array.push(array[i])
   end
   new_array
 end
